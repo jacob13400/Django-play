4672eab37906b3c6c07a7f55210c35cda6dfa78f
jacob13400 <jacob13400@gmail.com>
2019-06-01 12:30:28 +0530

    Updates

--- contact.py
+++ contact.py
@@ -1,7 +1,23 @@
 from django import forms
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 class ContactForm(forms.Form):
     yourname = forms.CharField(max_length=100,label="Your Name")
     email = forms.CharField(label="Your email id")
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
 
+def contact(request):
+     submitted = False
+     if request.method == 'POST':
+         form = ContactForm(request.POST)
+         if form.is_valid():
+             cd = form.cleaned_data
+             # assert False
+             return HttpResponseRedirect('/contact?submitted=True')
+     else:
+         form = ContactForm()
+         if 'submitted' in request.GET:
+             submitted = True
+ 
+     return render(request, 'contact/contact.html', {'form': form, 'submitted': submitted})
